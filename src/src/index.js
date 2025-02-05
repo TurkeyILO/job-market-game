757d79cbe8edd7c20eeb1aefc697646326f4dca5
TurkeyILO <turkey.ilo.hnmun@gmail.com>
2025-02-04 22:21:02 -0500

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import JobMarketGame from "./JobMarketGame";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <JobMarketGame />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
